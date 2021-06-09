fb7005d114bbf29a0af900b704b320cffcce393a
Serhii Kostiuchuk <serhiik@nayax.com>
2021-06-09 15:20:36 +0300

    initial commit

---
+++ borsch/src/main/java/com/nayax/borsch/BorschApplication.java
@@ -0,0 +1,14 @@
+package com.nayax.borsch;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.*;
+
+@SpringBootApplication
+public class BorschApplication {
+
+    public static void main(String[] args) {
+        run(BorschApplication.class, args);
+    }
+
+}
---
+++ borsch/src/test/java/com/nayax/borsch/BorschApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nayax.borsch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorschApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
