55c630d58abc12b8d79e4b373d49121a4740fb68
Nyamsty Kostya <reyzarium@gmail.com>
2021-06-15 09:56:28 +0300

    commiter

--- borsch/src/main/java/com/nayax/borsch/example/mapper/EmployeeMapper.java
+++ borsch/src/main/java/com/nayax/borsch/example/mapper/EmployeeMapper.java
@@ -4,9 +4,11 @@ import com.nayax.borsch.example.model.Employee;
 import com.nayax.borsch.example.model.EmployeeResponseDto;
 import com.nayax.borsch.example.model.dto.EmployeeAddDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface EmployeeMapper {
     EmployeeResponseDto toDto (Employee entity);
+    @Mapping(target = "id", ignore=true)
     Employee toEntity (EmployeeAddDto dto);
 }
--- borsch/src/main/java/com/nayax/borsch/exampleselezniov/mapper/EmployeeMapper.java
+++ borsch/src/main/java/com/nayax/borsch/exampleselezniov/mapper/EmployeeMapper.java
@@ -4,9 +4,11 @@ import com.nayax.borsch.exampleselezniov.model.dto.EmployeeAddDto;
 import com.nayax.borsch.exampleselezniov.model.dto.EmployeeResponseDto;
 import com.nayax.borsch.exampleselezniov.model.entity.Employee;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface EmployeeMapper {
     EmployeeResponseDto toDto(Employee entity);
+    @Mapping(target = "id", ignore=true)
     Employee toAddEntity(EmployeeAddDto dto);
 }
