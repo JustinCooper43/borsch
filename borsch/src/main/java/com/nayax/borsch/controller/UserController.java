dacf676959f8b7d6207591764f37b30a98bd8b28
oleksandr selezniov <olexandr.selezniov@gmail.com>
2021-06-15 14:53:12 +0300

    Controller update, delete, find all; Userfilter

--- borsch/src/main/java/com/nayax/borsch/controller/UserController.java
+++ borsch/src/main/java/com/nayax/borsch/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.nayax.borsch.controller;
 
 import com.nayax.borsch.model.dto.request.UserAddDto;
+import com.nayax.borsch.model.dto.request.UserUpdateDto;
 import com.nayax.borsch.model.dto.response.ResponseDto;
 import com.nayax.borsch.model.dto.response.user.UserResponseDto;
 import com.nayax.borsch.service.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -23,6 +26,24 @@ public class UserController {
     @GetMapping("/get")
     public ResponseEntity<ResponseDto<UserResponseDto>> get(@RequestParam Long id) {
         ResponseDto<UserResponseDto> responseDto = service.get(id);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<ResponseDto<UserResponseDto>> update(@RequestBody UserUpdateDto dto){
+        ResponseDto<UserResponseDto> responseDto = service.update(dto);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseDto<UserResponseDto>> delete(@RequestParam Long id) {
+        ResponseDto<UserResponseDto> responseDto = service.delete(id);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<ResponseDto<List<UserResponseDto>>> getAll() {
+        ResponseDto<List<UserResponseDto>> responseDto = service.getAll();
         return ResponseEntity.ok(responseDto);
     }
 }
