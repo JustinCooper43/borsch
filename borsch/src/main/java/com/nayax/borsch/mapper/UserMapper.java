97fe03ba4ce3a74eaf0f74266f8bc3f2740952b8
Nyamsty Kostya <reyzarium@gmail.com>
2021-06-15 14:51:54 +0300

    Update USer Mapper

--- borsch/src/main/java/com/nayax/borsch/mapper/UserMapper.java
+++ borsch/src/main/java/com/nayax/borsch/mapper/UserMapper.java
@@ -6,18 +6,36 @@ import com.nayax.borsch.model.dto.response.user.UserResponseDto;
 import com.nayax.borsch.model.entity.UserEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper
 public interface UserMapper {
 
     @Mapping(source = "roleName", target = "role.name")
     @Mapping(source = "roleId", target = "role.id")
+    @Mapping(expression = "java(entity.getFirstName()+ \" \" +entity.getLastName())", target = "name")
     UserResponseDto toDto(UserEntity entity);
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "phoneNumber", ignore = true)
     @Mapping(target = "deleted", ignore = true)
+    @Mapping(source = "name", target = "firstName", qualifiedByName = "splitFirstName")
+    @Mapping(source = "name", target = "lastName", qualifiedByName = "splitLastName")
     UserEntity toAddEntity(UserAddDto dto);
 
     UserEntity toUpdateEntity(UserUpdateDto dto);
+
+    @Named("splitFirstName")
+    public static String splitFirstName(String name) {
+        String[] strings = name.split(" ");
+        return strings[0];
+    }
+
+    @Named("splitLastName")
+    public static String splitLastName(String name) {
+        String[] strings = name.split(" ");
+        return strings.length > 1 ? strings[1] : "";
+    }
+
+
 }
