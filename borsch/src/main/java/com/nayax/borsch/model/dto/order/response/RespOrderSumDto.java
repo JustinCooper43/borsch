12c6a47e3dd1ae7497cb1e9640b71a9b8eb84479
Nyamsty Kostya <reyzarium@gmail.com>
2021-06-16 11:45:45 +0300

    new Resp Dto added

---
+++ borsch/src/main/java/com/nayax/borsch/model/dto/order/response/RespOrderSumDto.java
@@ -0,0 +1,66 @@
+package com.nayax.borsch.model.dto.order.response;
+
+import com.nayax.borsch.model.dto.user.response.RespUserDto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+public class RespOrderSumDto {
+
+    private RespUserDto user;
+    private List<RespOrderDto> orders;
+    private Long paymentType;
+    private BigDecimal amount;
+    private BigDecimal paidAmount;
+    private LocalDateTime orderDate;
+
+    public RespUserDto getUser() {
+        return user;
+    }
+
+    public void setUser(RespUserDto user) {
+        this.user = user;
+    }
+
+    public List<RespOrderDto> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<RespOrderDto> orders) {
+        this.orders = orders;
+    }
+
+    public Long getPaymentType() {
+        return paymentType;
+    }
+
+    public void setPaymentType(Long paymentType) {
+        this.paymentType = paymentType;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getPaidAmount() {
+        return paidAmount;
+    }
+
+    public void setPaidAmount(BigDecimal paidAmount) {
+        this.paidAmount = paidAmount;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+}
