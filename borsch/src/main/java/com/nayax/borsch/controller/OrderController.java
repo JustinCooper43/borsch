cf43a940121087278b085a5c15c2d56d92e4d829
oleksandr selezniov <olexandr.selezniov@gmail.com>
2021-06-16 16:12:49 +0300

    fully mocked OrderController

--- borsch/src/main/java/com/nayax/borsch/controller/OrderController.java
+++ borsch/src/main/java/com/nayax/borsch/controller/OrderController.java
@@ -2,10 +2,16 @@ package com.nayax.borsch.controller;
 
 import com.nayax.borsch.model.dto.ResponseDto;
 import com.nayax.borsch.model.dto.assortment.request.ReqOrderItemAddDto;
+import com.nayax.borsch.model.dto.order.response.RespOrderDeliveryDto;
 import com.nayax.borsch.model.dto.order.response.RespOrderDto;
+import com.nayax.borsch.model.dto.order.response.RespOrderSumDto;
+import com.nayax.borsch.model.dto.order.response.RespOrderSumInfoDto;
+import com.nayax.borsch.model.dto.user.response.RespUserDto;
+import com.nayax.borsch.model.dto.user.response.nested.RoleDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -24,6 +30,21 @@ public class OrderController {
         return orderItem;
     }
 
+    private RespUserDto getUserMock() {
+        RespUserDto user = new RespUserDto();
+        user.setId(14L);
+        user.setFirstName("Fname");
+        user.setLastName("Lname");
+        user.seteMail("adress@server.com");
+        RoleDto role = new RoleDto();
+        role.setId(2L);
+        role.setName("Cashier");
+        user.setRole(role);
+        user.setPhoneNumber("+380123456789");
+        return user;
+    }
+
+
     @PostMapping("/")
     public ResponseEntity<ResponseDto<RespOrderDto>> addOrder(@RequestBody ReqOrderItemAddDto dto) {
         RespOrderDto orderItem = getRespOrderMock();
@@ -33,7 +54,7 @@ public class OrderController {
 
     @GetMapping("/")
     public ResponseEntity<ResponseDto<List<RespOrderDto>>> getPagedOrders(
-            @RequestParam int page, @RequestParam int pageSize, @RequestParam Long userId, @RequestParam LocalDateTime dateTime) {
+            @RequestParam int page, @RequestParam int pageSize, @RequestParam Long userId, @RequestParam(required = false) LocalDateTime dateTime) {
         RespOrderDto orderItem = getRespOrderMock();
         List<RespOrderDto> itemList = List.of(orderItem, orderItem, orderItem, orderItem, orderItem, orderItem, orderItem);
         ResponseDto<List<RespOrderDto>> responseDto = new ResponseDto<>(itemList);
@@ -42,14 +63,53 @@ public class OrderController {
 
     @GetMapping("/history")
     public ResponseEntity<ResponseDto<List<RespOrderDto>>> getPagedHistory(
-            @RequestParam Long userId, @RequestParam int page, @RequestParam int pageSize){
+            @RequestParam Long userId, @RequestParam int page, @RequestParam int pageSize) {
         RespOrderDto orderItem = getRespOrderMock();
         List<RespOrderDto> itemList = List.of(orderItem, orderItem, orderItem, orderItem, orderItem, orderItem, orderItem);
         ResponseDto<List<RespOrderDto>> responseDto = new ResponseDto<>(itemList);
         return ResponseEntity.ok(responseDto);
     }
+
+    @GetMapping("/summary")
+    public ResponseEntity<ResponseDto<RespOrderSumDto>> getOrderSummary(@RequestBody LocalDateTime dateTime) {
+        RespOrderSumDto orderSumDto = new RespOrderSumDto();
+        RespOrderDto orderItem = getRespOrderMock();
+        List<RespOrderDto> itemList = List.of(orderItem, orderItem, orderItem, orderItem, orderItem, orderItem, orderItem);
+        orderSumDto.setOrderDate(LocalDateTime.now().minusMinutes(10));
+        orderSumDto.setOrders(itemList);
+        orderSumDto.setUser(getUserMock());
+        orderSumDto.setAmount(new BigDecimal("40.3"));
+        orderSumDto.setPaidAmount(new BigDecimal("40.2"));
+        orderSumDto.setPaymentType(2L);
+        ResponseDto<RespOrderSumDto> responseDto = new ResponseDto<>(orderSumDto);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @GetMapping("/summary/info")
+    public ResponseEntity<ResponseDto<RespOrderSumInfoDto>> getOrderInfo(@RequestBody LocalDateTime dateTime) {
+        RespOrderSumInfoDto info = new RespOrderSumInfoDto();
+        info.setPayAmount(new BigDecimal("5000"));
+        info.setPayCompleted(new BigDecimal("4000"));
+        info.setPayConfirmed(new BigDecimal("1000"));
+        info.setPayUnconfirmed(new BigDecimal("3000"));
+        ResponseDto<RespOrderSumInfoDto> responseDto = new ResponseDto<>(info);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @GetMapping("/delivery")
+    public ResponseEntity<ResponseDto<List<RespOrderDeliveryDto>>> getDelivery(@RequestBody LocalDateTime dateTime) {
+        RespOrderDeliveryDto deliveryInfo = new RespOrderDeliveryDto();
+        deliveryInfo.setOrder(getRespOrderMock());
+        deliveryInfo.setOrderDate(dateTime);
+        deliveryInfo.setQuantity(3L);
+        List<RespOrderDeliveryDto> pages = List.of(deliveryInfo, deliveryInfo, deliveryInfo, deliveryInfo,
+                deliveryInfo, deliveryInfo, deliveryInfo, deliveryInfo, deliveryInfo);
+        ResponseDto<List<RespOrderDeliveryDto>> responseDto = new ResponseDto<>(pages);
+        return ResponseEntity.ok(responseDto);
+    }
+
     @DeleteMapping("/")
-    public boolean deleteOrder(Long orderItemId){
+    public boolean deleteOrder(@RequestParam Long orderItemId) {
         return true;
     }
 }
