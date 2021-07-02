d1938353bc967aab3075effb3a4810a165d555ca
oleksandr selezniov <olexandr.selezniov@gmail.com>
2021-07-02 10:11:39 +0300

    wrap table names with escape braces

--- borsch/src/main/java/com/nayax/borsch/repository/impl/TablesType.java
+++ borsch/src/main/java/com/nayax/borsch/repository/impl/TablesType.java
@@ -2,16 +2,16 @@ package com.nayax.borsch.repository.impl;
 
 public enum TablesType {
 
-    EXTRAITEM("ExtraItem"),
-    ADDITION("Addition"),
-    REMARK("Remark"),
-    SHAWARMA("ShawarmaType"),
-    ORDER("Order"),
-    USER("User"),
-    ORDER_SUMMARY("OrderSummary"),
-    PAYMENT("Payment"),
-    CASHIER("Cashier"),
-    ROLE("Role");
+    EXTRAITEM("[ExtraItem]"),
+    ADDITION("[Addition]"),
+    REMARK("[Remark]"),
+    SHAWARMA("[ShawarmaType]"),
+    ORDER("[Order]"),
+    USER("[User]"),
+    ORDER_SUMMARY("[OrderSummary]"),
+    PAYMENT("[Payment]"),
+    CASHIER("[Cashier]"),
+    ROLE("[Role]");
 
     public  String nameTable;
 
