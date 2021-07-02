628723c202bf7c8170e3a7472c5cc9e11dbac963
oleksandr selezniov <olexandr.selezniov@gmail.com>
2021-07-02 10:14:25 +0300

    fix NPE at date string validation

--- borsch/src/main/java/com/nayax/borsch/validation/config/DateTimeValidatorConfig.java
+++ borsch/src/main/java/com/nayax/borsch/validation/config/DateTimeValidatorConfig.java
@@ -19,7 +19,7 @@ public class DateTimeValidatorConfig {
         dateTimeValidator.add(SimpleValidatorComponent.getComponents(List.of(ValidationAction.DATE),
                 obj -> obj != null && ((String) obj).length() == 10, "Date is too short", "date"));
         dateTimeValidator.add(SimpleValidatorComponent.getComponents(List.of(ValidationAction.DATE),
-                obj -> ((String) obj).matches("^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$"),
+                obj -> obj != null && ((String) obj).matches("^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$"),
                 "Invalid date format", "date"));
 
     }
